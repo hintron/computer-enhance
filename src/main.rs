d18394361667fafd0eadf381496a2f1122966b15
Michael Hinton <github@hinton.e4ward.com>
2023-06-23 14:20:35 -0600

    Print out bytes in an instruction stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Computer Enhance homework program!");
+    decode_mov();
 }
+
+fn decode_mov() {
+    println!("Decoding a mov instruction...");
+    let mut inst_stream: Vec<u8> = vec![];
+
+    // Create a test stream of instructions to decode
+    inst_stream.push(0x89);
+    inst_stream.push(0xD9);
+
+    for byte in inst_stream {
+        println!("{byte:#X} ({byte:#b})")
+    }
+}
+
