a6ab1e77346ff91721b99956721ce2104c45ce69
Michael Hinton <github@hinton.e4ward.com>
2023-06-25 01:51:01 -0600

    Adjust output so it's compilable assembly

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,13 @@ struct InstType {
 }
 
 fn main() -> io::Result<()> {
-    println!("Welcome to the Computer Enhance homework program!");
+    println!("; Welcome to the Computer Enhance homework program!");
 
     let args: Vec<String> = env::args().collect();
 
     let mut inst_stream: Vec<u8> = vec![];
     if args.len() < 2 {
-        println!("Decoding a test instruction stream...");
+        println!("; Decoding a test instruction stream...");
         // Create a test stream of instructions to decode
         // A mov instr
         inst_stream.push(0x89);
@@ -46,11 +46,12 @@ fn main() -> io::Result<()> {
         inst_stream.push(0xD9);
     } else {
         // Get the instruction stream from a file.
-        println!("Decoding instructions from file '{}'...", &args[1]);
+        println!("; Decoding instructions from file '{}'...", &args[1]);
         let mut file = File::open(&args[1])?;
         file.read_to_end(&mut inst_stream)?;
     }
-
+    
+    println!("bits 16");
     decode(inst_stream);
 
     Ok(())
