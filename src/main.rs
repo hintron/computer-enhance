29a70aa350140c5d6e1bf30673cc4861f43cfc8b
Hinton, Michael <michael.hinton@intel.com>
2023-06-30 16:09:34 -0600

    Remove test section from main

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,10 @@ fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     let mut inst_stream: Vec<u8> = vec![];
-    if args.len() < 2 {
-        println!("; Decoding a test instruction stream...");
-        // Create a test stream of instructions to decode
-        // A mov instr
-        inst_stream.push(0x89);
-        inst_stream.push(0xD9);
-        // Add bogus instr
-        inst_stream.push(0x00);
-        // Another mov instr
-        inst_stream.push(0x88);
-        inst_stream.push(0xD9);
-    } else {
-        // Get the instruction stream from a file.
-        println!("; Decoding instructions from file '{}'...", &args[1]);
-        let mut file = File::open(&args[1])?;
-        file.read_to_end(&mut inst_stream)?;
-    }
+    // Get the instruction stream from a file.
+    println!("; Decoding instructions from file '{}'...", &args[1]);
+    let mut file = File::open(&args[1])?;
+    file.read_to_end(&mut inst_stream)?;
 
     println!("bits 16");
     let _insts = decode(inst_stream);
