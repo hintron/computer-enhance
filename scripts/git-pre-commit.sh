aaddca860fabf0a3a3296fbe67979fa1262bf62c
Michael Hinton <github@hinton.e4ward.com>
2023-07-01 22:19:29 -0600

    Simplify Git pre-commit script

--- scripts/git-pre-commit.sh
+++ scripts/git-pre-commit.sh
@@ -5,8 +5,7 @@
 # Redirect all output to stderr
 exec 1>&2
 # Run rustfmt on the project
-cargo fmt -- --check > /dev/null
-if [ "$?" == "1" ]; then
+if ! cargo fmt -- --check > /dev/null; then
     echo "Commit ABORTED: Please run \`cargo fmt\`."
     echo "Output of \`cargo fmt -- --check\`:"
     # Run `cargo fmt -- --check` to see what rustfmt would change
