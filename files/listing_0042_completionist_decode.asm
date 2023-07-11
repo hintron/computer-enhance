4eab5efd63432551e92b8808351935dd5a7f8f70
Michael Hinton <github@hinton.e4ward.com>
2023-07-10 23:09:14 -0600

    Uncomment already-implemented dec variants

--- files/listing_0042_completionist_decode.asm
+++ files/listing_0042_completionist_decode.asm
@@ -183,12 +183,12 @@ dec al
 dec ah
 ; dec sp
 ; dec di
-; dec byte [bp + 1002]
-; dec word [bx + 39]
-; dec byte [bx + si + 5]
-; dec word [bp + di - 10044]
-; dec word [9349]
-; dec byte [bp]
+dec byte [bp + 1002]
+dec word [bx + 39]
+dec byte [bx + si + 5]
+dec word [bp + di - 10044]
+dec word [9349]
+dec byte [bp]
 
 ; neg ax
 ; neg cx
