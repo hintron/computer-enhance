7d3aac7e347d15c129dbb46a2bff62e5e95a94de
Michael Hinton <github@hinton.e4ward.com>
2023-07-11 01:52:35 -0600

    Uncomment already-implemented conditional jumps

--- files/listing_0042_completionist_decode.asm
+++ files/listing_0042_completionist_decode.asm
@@ -355,27 +355,27 @@ jmp [4395]
 ; ret 500
 ; ret
     
-; label:
-; je label
-; jl label
-; jle label
-; jb label
-; jbe label
-; jp label
-; jo label
-; js label
-; jne label
-; jnl label
-; jg label
-; jnb label
-; ja label
-; jnp label
-; jno label
-; jns label
-; loop label
-; loopz label
-; loopnz label
-; jcxz label
+label:
+je label
+jl label
+jle label
+jb label
+jbe label
+jp label
+jo label
+js label
+jne label
+jnl label
+jg label
+jnb label
+ja label
+jnp label
+jno label
+jns label
+loop label
+loopz label
+loopnz label
+jcxz label
     
 ; int 13
 ; int3
