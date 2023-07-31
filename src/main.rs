e50b07b157f2c721747bd477b56fdaf240070a73
Hinton, Michael <michael.hinton@intel.com>
2023-07-31 17:15:10 -0600

    Split off arg parsing into separate function

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,29 @@ mod tests;
 // Internal imports
 use crate::decode::decode;
 
-fn main() -> io::Result<()> {
-    println!("Welcome to the Computer Enhance homework program!");
-
+/// Parse command line arguments.
+/// Return a tuple with the following contents (in order):
+/// * input File object to decode
+/// * output File object to output decoded assembly to
+fn parse_args() -> io::Result<(File, File)> {
     let args: Vec<String> = env::args().collect();
-
-    let mut inst_stream: Vec<u8> = vec![];
     // Get the instruction stream from a file.
     println!("Decoding instructions from file '{}'...", &args[1]);
-    let mut file = File::open(&args[1])?;
+    let file = File::open(&args[1])?;
     println!("Outputting decoded assembly to file '{}'...", &args[2]);
-    let mut output = OpenOptions::new()
+    let output = OpenOptions::new()
         .write(true)
         .create_new(true)
         .open(&args[2])?;
-    file.read_to_end(&mut inst_stream)?;
+    Ok((file, output))
+}
 
+fn main() -> io::Result<()> {
+    println!("Welcome to the Computer Enhance homework program!");
+
+    let (mut file, mut output) = parse_args()?;
+    let mut inst_stream: Vec<u8> = vec![];
+    file.read_to_end(&mut inst_stream)?;
     let insts = decode(inst_stream);
 
     // Print out instructions to the output file
