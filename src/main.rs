f68efc72640235daf85678658f2945c15ea23220
Hinton, Michael <michael.hinton@intel.com>
2023-07-31 18:25:06 -0600

    Create an arg struct and return it from parse_args()

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,17 @@ mod tests;
 // Internal imports
 use crate::decode::decode;
 
+/// A custom struct holding parsed command line arguments
+struct ArgsType {
+    /// The File object to decode
+    input_file: File,
+    /// The File object to output decoded assembly to
+    output_file: File,
+}
+
 /// Parse command line arguments.
-/// Return a tuple with the following contents (in order):
-/// * input File object to decode
-/// * output File object to output decoded assembly to
-fn parse_args() -> io::Result<(File, File)> {
+/// Return an ArgsType struct
+fn parse_args() -> io::Result<ArgsType> {
     let args: Vec<String> = env::args().collect();
     // Get the instruction stream from a file.
     println!("Decoding instructions from file '{}'...", &args[1]);
@@ -31,21 +37,24 @@ fn parse_args() -> io::Result<(File, File)> {
         .write(true)
         .create_new(true)
         .open(&args[2])?;
-    Ok((file, output))
+    Ok(ArgsType {
+        input_file: file,
+        output_file: output,
+    })
 }
 
 fn main() -> io::Result<()> {
     println!("Welcome to the Computer Enhance homework program!");
 
-    let (mut file, mut output) = parse_args()?;
+    let mut args = parse_args()?;
     let mut inst_stream: Vec<u8> = vec![];
-    file.read_to_end(&mut inst_stream)?;
+    args.input_file.read_to_end(&mut inst_stream)?;
     let insts = decode(inst_stream);
 
     // Print out instructions to the output file
-    writeln!(output, "bits 16")?;
+    writeln!(args.output_file, "bits 16")?;
     for inst in insts {
-        writeln!(output, "{}", inst.text.unwrap())?;
+        writeln!(args.output_file, "{}", inst.text.unwrap())?;
     }
 
     Ok(())
