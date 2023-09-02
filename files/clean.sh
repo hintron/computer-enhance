eb8461fef005f0a512d076aea02e56f882a74e93
Michael Hinton <github@hinton.e4ward.com>
2023-09-01 23:19:34 -0600

    Add simulation regress infrastructure

--- files/clean.sh
+++ files/clean.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BUILD_DIR="$SCRIPT_DIR/build-decode-regress"
+BUILD_DIR_DECODE="$SCRIPT_DIR/build-decode-regress"
+BUILD_DIR_SIMULATE="$SCRIPT_DIR/build-simulate-regress"
 
-rm -rf "$BUILD_DIR"
+rm -rf "$BUILD_DIR_DECODE"
+rm -rf "$BUILD_DIR_SIMULATE"
--- scripts/regress.sh
+++ scripts/regress.sh
@@ -3,7 +3,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$(realpath "$SCRIPT_DIR/..")
 FILE_DIR="$PROJECT_DIR/files"
-ASM_BUILD_DIR="$FILE_DIR/build-decode-regress"
+DECODE_BUILD_DIR="$FILE_DIR/build-decode-regress"
+SIMULATE_BUILD_DIR="$FILE_DIR/build-simulate-regress"
+SIMULATE_SRC_DIR="$FILE_DIR/simulate-regress"
 BIN="$PROJECT_DIR/target/debug/computer-enhance"
 
 CHECK_RTOS="false"
@@ -14,6 +16,7 @@ fi
 DATE=$(date +"%Y-%m-%d at %H:%M:%S")
 echo "Date: $DATE"
 
+# Build everything in decode-regress and simulate-regress
 cd $FILE_DIR
 $FILE_DIR/clean.sh
 if ! $FILE_DIR/make.sh; then
@@ -26,21 +29,23 @@ if ! cargo build; then
 fi
 
 rc=0
-for file in "$ASM_BUILD_DIR"/*; do
+
+# Check the decode of everything in decode-regress
+for file in "$DECODE_BUILD_DIR"/*; do
     echo "Checking decode of $file..."
     BASE=$(basename $file)
-    if ! $BIN "$file" "$ASM_BUILD_DIR/$BASE-tmp.asm" > "$ASM_BUILD_DIR/$BASE-tmp.log"; then
+    if ! $BIN "$file" "$DECODE_BUILD_DIR/$BASE-tmp.asm" > "$DECODE_BUILD_DIR/$BASE-tmp.log"; then
         echo "ERROR: Decode program failed for $file"
         rc=1
         break
     fi
-    nasm "$ASM_BUILD_DIR/$BASE-tmp.asm" -o "$ASM_BUILD_DIR/$BASE-tmp.o"
-    if ! diff "$ASM_BUILD_DIR/$BASE-tmp.o" "$file" ; then
+    nasm "$DECODE_BUILD_DIR/$BASE-tmp.asm" -o "$DECODE_BUILD_DIR/$BASE-tmp.o"
+    if ! diff "$DECODE_BUILD_DIR/$BASE-tmp.o" "$file" ; then
         echo "ERROR: Assembly of decoded output failed for $file"
         rc=1
         break
     fi
-    if ! diff "$ASM_BUILD_DIR/$BASE-tmp.o" "$file" ; then
+    if ! diff "$DECODE_BUILD_DIR/$BASE-tmp.o" "$file" ; then
         echo "ERROR: Decoded output didn't match golden for $file"
         rc=1
         break
@@ -72,7 +77,7 @@ while [ "$CHECK_RTOS" == "true" ]; do
     RTOS_DIR="$RTOS_REPO/labs/lab8"
     RTOS_ASM="$RTOS_DIR/artossfinal.s"
     RTOS_BIN="$RTOS_DIR/artoss.bin"
-    RTOS_BIN_TRUNC="$ASM_BUILD_DIR/artoss.bin.truncated"
+    RTOS_BIN_TRUNC="$DECODE_BUILD_DIR/artoss.bin.truncated"
     cd $RTOS_DIR
     make clean
     if ! make; then
@@ -88,23 +93,47 @@ while [ "$CHECK_RTOS" == "true" ]; do
 
     echo "Checking decode of $RTOS_BIN_TRUNC..."
     BASE=$(basename $RTOS_BIN_TRUNC)
-    if ! $BIN "$RTOS_BIN_TRUNC" "$ASM_BUILD_DIR/$BASE-tmp.asm" > "$ASM_BUILD_DIR/$BASE-tmp.log"; then
+    if ! $BIN "$RTOS_BIN_TRUNC" "$DECODE_BUILD_DIR/$BASE-tmp.asm" > "$DECODE_BUILD_DIR/$BASE-tmp.log"; then
         echo "ERROR: Decode program failed for $RTOS_BIN_TRUNC"
         rc=1
         break
     fi
-    nasm "$ASM_BUILD_DIR/$BASE-tmp.asm" -o "$ASM_BUILD_DIR/$BASE-tmp.o"
-    if ! diff "$ASM_BUILD_DIR/$BASE-tmp.o" "$file" ; then
+    nasm "$DECODE_BUILD_DIR/$BASE-tmp.asm" -o "$DECODE_BUILD_DIR/$BASE-tmp.o"
+    if ! diff "$DECODE_BUILD_DIR/$BASE-tmp.o" "$file" ; then
         echo "ERROR: Assembly of decoded output failed for $file"
         rc=1
         break
     fi
-    if ! diff "$ASM_BUILD_DIR/$BASE-tmp.o" "$file" ; then
+    if ! diff "$DECODE_BUILD_DIR/$BASE-tmp.o" "$file" ; then
         echo "ERROR: Decoded output didn't match golden for $file"
         rc=1
         break
     fi
 done
+
+
+# Check the decode and simulation of everything in simulate-regress
+for file in "$SIMULATE_BUILD_DIR"/*; do
+    # If the glob found nothing, it will be treated as a file, so skip it 
+    if [ ! -e "$file" ]; then continue; fi
+    echo "Checking simulation of $file..."
+    BASE=$(basename "$file")
+    SIMULATE_OUTPUT="$SIMULATE_BUILD_DIR/$BASE-simulate.txt"
+    SIMULATE_LOG="$SIMULATE_BUILD_DIR/$BASE-simulate.log"
+    if ! $BIN "$file" "$SIMULATE_OUTPUT" --exec > "$SIMULATE_LOG"; then
+        echo "ERROR: Simulation of program failed for $file"
+        rc=1
+        break
+    fi
+    SIMULATE_GOLDEN_OUTPUT="$SIMULATE_SRC_DIR/$BASE.txt"
+    if ! diff "$SIMULATE_GOLDEN_OUTPUT" "$SIMULATE_OUTPUT" ; then
+        echo "ERROR: Simulation output didn't match golden output"
+        echo "$SIMULATE_OUTPUT != $SIMULATE_GOLDEN_OUTPUT"
+        rc=1
+        break
+    fi
+done
+
 
 if [ "$rc" == "0" ]; then
     echo "All regressions passed"
