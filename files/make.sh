9a15d46aabf1e322b6e633827a6ff2233078b6cd
Michael Hinton <github@hinton.e4ward.com>
2023-09-01 23:19:34 -0600

    Add simulation regress infrastructure

--- files/make.sh
+++ files/make.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BUILD_DIR="$SCRIPT_DIR/build-decode-regress"
+BUILD_DIR_DECODE="$SCRIPT_DIR/build-decode-regress"
+BUILD_DIR_SIMULATE="$SCRIPT_DIR/build-simulate-regress"
 
 cd "$SCRIPT_DIR"
-mkdir -p "$BUILD_DIR"
-# Make each asm file with nasm
+mkdir -p "$BUILD_DIR_DECODE"
+mkdir -p "$BUILD_DIR_SIMULATE"
+
+# Build each decode asm file with nasm
 for file in "$SCRIPT_DIR"/decode-regress/*.asm; do
     if [ -f "$file" ]; then
         new_name=$(basename "${file%.*}")
-        echo "Assembling $new_name"
-        if ! nasm "$file" -o "$BUILD_DIR/$new_name"; then
+        echo "Assembling (decode) $new_name"
+        if ! nasm "$file" -o "$BUILD_DIR_DECODE/$new_name"; then
+            echo "ERROR: nasm failed!"
+            exit 1
+        fi
+    fi
+done
+
+# Build each simulate asm file with nasm
+for file in "$SCRIPT_DIR"/simulate-regress/*.asm; do
+    if [ -f "$file" ]; then
+        new_name=$(basename "${file%.*}")
+        echo "Assembling (simulate) $new_name"
+        if ! nasm "$file" -o "$BUILD_DIR_SIMULATE/$new_name"; then
             echo "ERROR: nasm failed!"
             exit 1
         fi
