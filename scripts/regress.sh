28e2e08cc2b7b6fea2342e58361569c093c4399d
Michael Hinton <michael.hinton@intel.com>
2024-02-24 15:59:10 -0700

    Move RTOS regression to bottom of regress.sh

--- scripts/regress.sh
+++ scripts/regress.sh
@@ -71,69 +71,6 @@ for file in "$DECODE_BUILD_DIR"/*; do
     rm "$DIFF"
 done
 
-# TODO: Assemble individual files, not final rtos file
-# TODO: In handlers_simptris.s, there is:
-    # L_handlers_simptris_5:
-    # 	DB	".",0
-# This looks like an 0x2E, which is an ES segment override prefix (not sure what that does)
-# So the problem is that my decoder can't know that this is data rather than an
-# instruction. But NASM does when it assembles it. So perhaps this blind decode
-# may not work well.
-# How do real decoders handle data bytes?
-
-# Use while loop to easily break out
-while [ "$CHECK_RTOS" == "true" ]; do
-    # Try decoding my ECEn 425 RTOS
-    if [ -d "$HOME/code/425_artoss/" ]; then
-        RTOS_REPO="$HOME/code/425_artoss/"
-    elif [ -d "$HOME/code/artoss/" ]; then
-        RTOS_REPO="$HOME/code/artoss/"
-    else
-        echo "ERROR: Unable to find RTOS dir!"
-        rc=1
-        break
-    fi
-    RTOS_DIR="$RTOS_REPO/labs/lab8"
-    RTOS_BIN="$RTOS_DIR/artoss.bin"
-    RTOS_BIN_TRUNC="$DECODE_BUILD_DIR/artoss.bin.truncated"
-    cd "$RTOS_DIR" || exit
-    make clean
-    if ! make; then
-        echo "ERROR: Failed to build RTOS"
-        rc=1
-        break
-    fi
-    cd "$FILE_DIR" || exit
-
-    # https://unix.stackexchange.com/questions/13907/delete-the-first-n-bytes-of-files
-    echo "Stripping off the first 100 data bytes $RTOS_BIN..."
-    tail +257c "$RTOS_BIN" > "$RTOS_BIN_TRUNC"
-
-    echo "Checking decode of $RTOS_BIN_TRUNC..."
-    BASE=$(basename "$RTOS_BIN_TRUNC")
-    if ! $BIN --verbose "$RTOS_BIN_TRUNC" "$DECODE_BUILD_DIR/$BASE-tmp.asm" > "$DECODE_BUILD_DIR/$BASE-tmp.log"; then
-        echo "ERROR: Decode program failed for $RTOS_BIN_TRUNC"
-        rc=1
-        break
-    fi
-    if ! nasm "$DECODE_BUILD_DIR/$BASE-tmp.asm" -o "$DECODE_BUILD_DIR/$BASE-tmp.o"; then
-        echo "ERROR: Assembly of decoded output failed for $file"
-        rc=1
-        break
-    fi
-    RTOS_GOLDEN_OUTPUT="$DECODE_BUILD_DIR/$BASE-tmp.o"
-    DIFF="$DECODE_BUILD_DIR/$BASE-rtos.diff"
-    if ! diff "$RTOS_GOLDEN_OUTPUT" "$file" -u > "$DIFF"; then
-        echo "ERROR: Decoded RTOS output didn't match golden output."
-        echo "See $DIFF"
-        echo "ours   (+): $file"
-        echo "golden (-): $RTOS_GOLDEN_OUTPUT"
-        rc=1
-        break
-    fi
-    rm "$DIFF"
-done
-
 
 # Check the decode and simulation of everything in simulate-regress, but without
 # checking the IP register.
@@ -255,6 +192,71 @@ for file in "$SIMULATE_8086_BUILD_DIR"/*; do
     fi
     rm "$DIFF"
 done
+
+
+# TODO: Assemble individual files, not final rtos file
+# TODO: In handlers_simptris.s, there is:
+    # L_handlers_simptris_5:
+    # 	DB	".",0
+# This looks like an 0x2E, which is an ES segment override prefix (not sure what that does)
+# So the problem is that my decoder can't know that this is data rather than an
+# instruction. But NASM does when it assembles it. So perhaps this blind decode
+# may not work well.
+# How do real decoders handle data bytes?
+
+# Use while loop to easily break out
+while [ "$CHECK_RTOS" == "true" ]; do
+    # Try decoding my ECEn 425 RTOS
+    if [ -d "$HOME/code/425_artoss/" ]; then
+        RTOS_REPO="$HOME/code/425_artoss/"
+    elif [ -d "$HOME/code/artoss/" ]; then
+        RTOS_REPO="$HOME/code/artoss/"
+    else
+        echo "ERROR: Unable to find RTOS dir!"
+        rc=1
+        break
+    fi
+    RTOS_DIR="$RTOS_REPO/labs/lab8"
+    RTOS_BIN="$RTOS_DIR/artoss.bin"
+    RTOS_BIN_TRUNC="$DECODE_BUILD_DIR/artoss.bin.truncated"
+    cd "$RTOS_DIR" || exit
+    make clean
+    if ! make; then
+        echo "ERROR: Failed to build RTOS"
+        rc=1
+        break
+    fi
+    cd "$FILE_DIR" || exit
+
+    # https://unix.stackexchange.com/questions/13907/delete-the-first-n-bytes-of-files
+    echo "Stripping off the first 100 data bytes $RTOS_BIN..."
+    tail +257c "$RTOS_BIN" > "$RTOS_BIN_TRUNC"
+
+    echo "Checking decode of $RTOS_BIN_TRUNC..."
+    BASE=$(basename "$RTOS_BIN_TRUNC")
+    if ! $BIN --verbose "$RTOS_BIN_TRUNC" "$DECODE_BUILD_DIR/$BASE-tmp.asm" > "$DECODE_BUILD_DIR/$BASE-tmp.log"; then
+        echo "ERROR: Decode program failed for $RTOS_BIN_TRUNC"
+        rc=1
+        break
+    fi
+    if ! nasm "$DECODE_BUILD_DIR/$BASE-tmp.asm" -o "$DECODE_BUILD_DIR/$BASE-tmp.o"; then
+        echo "ERROR: Assembly of decoded output failed for $file"
+        rc=1
+        break
+    fi
+    RTOS_GOLDEN_OUTPUT="$DECODE_BUILD_DIR/$BASE-tmp.o"
+    DIFF="$DECODE_BUILD_DIR/$BASE-rtos.diff"
+    if ! diff "$RTOS_GOLDEN_OUTPUT" "$file" -u > "$DIFF"; then
+        echo "ERROR: Decoded RTOS output didn't match golden output."
+        echo "See $DIFF"
+        echo "ours   (+): $file"
+        echo "golden (-): $RTOS_GOLDEN_OUTPUT"
+        rc=1
+        break
+    fi
+    rm "$DIFF"
+done
+
 
 if [ "$rc" == "0" ]; then
     echo "All regressions passed"
