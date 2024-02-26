f5f9d0096c609791f4b3ed8dbfba326a633b70cd
Michael Hinton <michael.hinton@intel.com>
2024-02-26 16:50:38 -0700

    WIP: Start decoding Snake game in regressions
    It is tricky to try to assemble both because of the data parts...

--- emulator/files/make.sh
+++ emulator/files/make.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BUILD_DIR_DECODE="$SCRIPT_DIR/build-decode-regress"
 SRC_DIR_DECODE="$SCRIPT_DIR/decode-regress"
+BUILD_DIR_SNAKE="$SCRIPT_DIR/build-snake-regress"
+SRC_DIR_SNAKE="$SCRIPT_DIR/snake-regress"
 BUILD_DIR_SIMULATE="$SCRIPT_DIR/build-simulate-regress"
 SRC_DIR_SIMULATE="$SCRIPT_DIR/simulate-regress"
 BUILD_DIR_SIMULATE_IP="$SCRIPT_DIR/build-simulate-ip-regress"
@@ -14,6 +16,7 @@ SRC_DIR_SIMULATE_8086="$SCRIPT_DIR/simulate-8086-regress"
 
 cd "$SCRIPT_DIR" || exit
 mkdir -p "$BUILD_DIR_DECODE"
+mkdir -p "$BUILD_DIR_SNAKE"
 mkdir -p "$BUILD_DIR_SIMULATE"
 mkdir -p "$BUILD_DIR_SIMULATE_IP"
 mkdir -p "$BUILD_DIR_SIMULATE_CYCLES"
@@ -25,6 +28,22 @@ for file in "$SRC_DIR_DECODE"/*.asm; do
         new_name=$(basename "${file%.*}")
         echo "Assembling (decode) $new_name"
         if ! nasm "$file" -o "$BUILD_DIR_DECODE/$new_name"; then
+            echo "ERROR: nasm failed!"
+            exit 1
+        fi
+    fi
+done
+
+# Build each Snake asm file with nasm
+for file in "$SRC_DIR_SNAKE"/*.asm; do
+    if [ -f "$file" ]; then
+        new_name=$(basename "${file%.*}")
+        echo "Assembling (snake) $new_name"
+
+        # # Delete everything after the first occurrence of int3
+        # sed -i '/int3/q' $file
+
+        if ! nasm "$file" -o "$BUILD_DIR_SNAKE/$new_name"; then
             echo "ERROR: nasm failed!"
             exit 1
         fi
