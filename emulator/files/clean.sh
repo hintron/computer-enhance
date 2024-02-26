41fd71f6153fe6eda94b9a23441c1510cfd5ce9c
Michael Hinton <michael.hinton@intel.com>
2024-02-26 16:50:38 -0700

    WIP: Start decoding Snake game in regressions
    It is tricky to try to assemble both because of the data parts...

--- emulator/files/clean.sh
+++ emulator/files/clean.sh
@@ -2,12 +2,14 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BUILD_DIR_DECODE="$SCRIPT_DIR/build-decode-regress"
+BUILD_DIR_SNAKE="$SCRIPT_DIR/build-snake-regress"
 BUILD_DIR_SIMULATE="$SCRIPT_DIR/build-simulate-regress"
 BUILD_DIR_SIMULATE_IP="$SCRIPT_DIR/build-simulate-ip-regress"
 BUILD_DIR_SIMULATE_CYCLES="$SCRIPT_DIR/build-simulate-ip-cycles-regress"
 BUILD_DIR_8086="$SCRIPT_DIR/build-simulate-8086-regress"
 
 rm -rf "$BUILD_DIR_DECODE"
+rm -rf "$BUILD_DIR_SNAKE"
 rm -rf "$BUILD_DIR_SIMULATE"
 rm -rf "$BUILD_DIR_SIMULATE_IP"
 rm -rf "$BUILD_DIR_SIMULATE_CYCLES"
