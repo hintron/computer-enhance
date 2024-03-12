e0b486cf3d6130f6a014d5843bb6a4d2cc256a75
Michael Hinton <michael.hinton@intel.com>
2024-03-12 12:33:09 -0600

    Have plus/up and minus/down scale image up or down, respectively

--- emu86rs/src/display.rs
+++ emu86rs/src/display.rs
@@ -14,8 +14,9 @@ use std::num::NonZeroU32;
 use std::rc::Rc;
 
 // Third-party imports
-use winit::event::{Event, WindowEvent};
+use winit::event::{ElementState, Event, KeyEvent, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
+use winit::keyboard::Key;
 use winit::keyboard::NamedKey;
 use winit::window::WindowBuilder;
 
@@ -53,6 +54,10 @@ pub fn display_memory(memory: &[u8], image_width: u32, image_height: u32) {
     let memory_u32: &[u32] =
         unsafe { std::slice::from_raw_parts(memory.as_ptr() as *const u32, memory.len() / 4) };
 
+    // Remember scale state
+    let mut scale_req = 0;
+    let mut scale_max = 1;
+
     let result = event_loop.run(move |event, elwt| {
         elwt.set_control_flow(ControlFlow::Wait);
 
@@ -82,7 +87,15 @@ pub fn display_memory(memory: &[u8], image_width: u32, image_height: u32) {
                 // MGH TODO: Try branchless programming to remove if
                 let mut row: u32 = 0;
                 let mut index = 0;
-                let scale = cmp::min(width / image_width, height / image_height);
+                scale_max = cmp::min(width / image_width, height / image_height);
+                let scale = if scale_req == 0 || scale_req > scale_max {
+                    println!("Max scale: {scale_max}");
+                    scale_req = scale_max;
+                    scale_max
+                } else {
+                    println!("Req scale: {scale_req}");
+                    scale_req
+                };
                 let width_scaled = image_width * scale;
                 let height_scaled = image_height * scale;
                 let mut row_repeat = scale;
@@ -132,12 +145,44 @@ pub fn display_memory(memory: &[u8], image_width: u32, image_height: u32) {
             Event::WindowEvent {
                 event:
                     WindowEvent::KeyboardInput {
-                        event: key_event, ..
+                        event:
+                            KeyEvent {
+                                logical_key: key,
+                                state: ElementState::Pressed,
+                                ..
+                            },
+                        ..
                     },
                 window_id,
-            } if window_id == window.id() && key_event.logical_key == NamedKey::Escape => {
-                println!("Escape key pressed! Exiting...");
-                elwt.exit();
+            } if window_id == window.id() => {
+                let mut increase_scale = false;
+                let mut decrease_scale = false;
+                match key {
+                    Key::Named(NamedKey::Escape) => {
+                        println!("Escape key pressed! Exiting...");
+                        elwt.exit();
+                    }
+                    Key::Named(NamedKey::ArrowUp) => increase_scale = true,
+                    Key::Named(NamedKey::ArrowDown) => decrease_scale = true,
+                    Key::Character(char) => {
+                        if char.starts_with("+") {
+                            increase_scale = true;
+                        } else if char.starts_with("-") {
+                            decrease_scale = true;
+                        }
+                    }
+                    other => println!("{other:#?}"),
+                }
+                if increase_scale && scale_req < scale_max {
+                    scale_req += 1;
+                    window.request_redraw();
+                    println!("Increasing requested scale to: {scale_req}");
+                }
+                if decrease_scale && scale_req > 1 {
+                    scale_req -= 1;
+                    window.request_redraw();
+                    println!("Decreasing requested scale to: {scale_req}");
+                }
             }
             event => println!("Window event: {event:?}"),
         }
