56108012972f2576ff5b9c02e7d05c25e2cf88b5
Michael Hinton <github@hinton.e4ward.com>
2024-03-28 01:52:21 -0600

    Start generating a haversine pairs json

--- haversine/src/main.rs
+++ haversine/src/main.rs
@@ -3,6 +3,19 @@
 //! This is for part 2 of the Computer Enhance programming course at
 //! https://www.computerenhance.com/.
 
+// Standard library imports
+// External imports
+// Internal imports
+
 fn main() {
-    println!("Hello, world, from the haversine processor!");
+    println!("{{\"pairs\": [");
+    // for i in 0..10_000_000 {
+    for i in 0..10_000 {
+        let x0 = i as f64;
+        let y0 = i as f64;
+        let x1 = i as f64;
+        let y1 = i as f64;
+        println!("    {{\"x0\": {x0}, \"y0\": {y0}, \"x1\": {x1}, \"y1\": {y1}}},");
+    }
+    println!("]}}");
 }
