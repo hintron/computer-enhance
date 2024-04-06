597311ed31d27d76017db97406aa76627f056dea
Michael Hinton <github@hinton.e4ward.com>
2024-04-06 01:40:26 -0600

    Make haversine generator take in an input count

--- haversine/src/main.rs
+++ haversine/src/main.rs
@@ -4,21 +4,35 @@
 //! https://www.computerenhance.com/.
 
 // Standard library imports
+use std::env;
 // External imports
-// Internal imports
+use anyhow::Result;
 
-fn main() {
+// Internal imports
+const DEFAULT_COUNT: u64 = 10_000_000;
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut count = 0;
+    for arg in &args[1..] {
+        if count == 0 {
+            count = arg.parse()?;
+        }
+    }
+    if count == 0 {
+        count = DEFAULT_COUNT;
+    }
+
     println!("{{\"pairs\": [");
-    let max = 10;
-    // let max = 10_000;
-    // max = 10_000_000;
-    for i in 0..max {
+    for i in 0..count {
         let x0 = i as f64;
         let y0 = i as f64;
         let x1 = i as f64;
         let y1 = i as f64;
-        let comma = if i == (max - 1) { "" } else { "," };
+        let comma = if i == (count - 1) { "" } else { "," };
         println!("    {{\"x0\": {x0}, \"y0\": {y0}, \"x1\": {x1}, \"y1\": {y1}}}{comma}");
     }
     println!("]}}");
+
+    Ok(())
 }
